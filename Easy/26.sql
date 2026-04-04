5ac3c9618f0c5567de50227410325c71f0d4ff3e
Jerald jenid J <jeraldjenid17@gmail.com>
2026-04-04 10:26:54 +0530

    Bank Account Summary 2

---
+++ Easy/26.sql
@@ -0,0 +1,9 @@
+-- Bank Account Summary 2
+
+SELECT 
+    u.name, 
+    SUM(t.amount) AS balance
+FROM Users u
+JOIN Transactions t ON u.account = t.account
+GROUP BY u.account
+HAVING SUM(t.amount) > 10000;
